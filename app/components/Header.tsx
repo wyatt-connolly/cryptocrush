338fb81bc5a21815081bd22a589c2bbcc8e318b2
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-04-14 18:09:39 -0700

    nav and footer

---
+++ app/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow-sm">
+      <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
+        <h1 className="text-lg font-semibold leading-6 text-gray-900">
+          Dashboard
+        </h1>
+      </div>
+    </header>
+  );
+}
