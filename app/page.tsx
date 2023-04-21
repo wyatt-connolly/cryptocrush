38dd48844ee9db70886fc422b57922d03da4a2fa
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-04-20 17:30:54 -0700

    more type fix

--- app/page.tsx
+++ app/page.tsx
@@ -27,20 +27,6 @@ import Error from "./components/Error";
 import SlideOver from "./components/SlideOver";
 import fetcher from "./lib/utils";
 
-type Coin = {
-  id: string;
-  name: string;
-  symbol: string;
-  image: string;
-  current_price: number;
-  market_cap: number;
-  market_cap_rank: number;
-  total_volume: number;
-  high_24h: number;
-  low_24h: number;
-  price_change_percentage_24h: number;
-};
-
 type Category = {
   id: string;
   name: string;
@@ -216,8 +202,22 @@ function Join() {
   );
 }
 
+type Coin = {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  market_cap: number;
+  market_cap_rank: number;
+  total_volume: number;
+  high_24h: number;
+  low_24h: number;
+  price_change_percentage_24h: number;
+};
+
 export default function Home() {
-  const [currentPage, setCurrentPage] = useState([1]);
+  const [currentPage, setCurrentPage] = useState(1);
   const { data, error, isLoading } = useSWR(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=${currentPage}&sparkline=false
     `,
