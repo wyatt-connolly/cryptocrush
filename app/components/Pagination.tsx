b664d9604058d6da64e1d4c24e5c6b8d3d27cd87
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-04-20 18:06:51 -0700

    scroll fix

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -14,22 +14,6 @@ export default function Pagination({
   currentPage,
   setCurrentPage,
 }: IPagination) {
-  const paginationButtons = [];
-  for (let i = 1; i <= 5; i++) {
-    paginationButtons.push(
-      <button
-        key={i}
-        onClick={() => setCurrentPage(i)}
-        className={classNames(
-          "inline-flex items-center px-4 pt-4 text-sm font-medium text-gray-500 border-t-2 border-transparent hover:border-gray-300 hover:text-gray-700 focus:border-t-indigo-500 focus:text-indigo-600",
-          currentPage === i && "border-indigo-500 text-indigo-600"
-        )}
-      >
-        {i}
-      </button>
-    );
-  }
-
   const scrollMarket = () => {
     window.scrollTo({
       top: window.pageYOffset - 800,
@@ -37,16 +21,29 @@ export default function Pagination({
     });
   };
 
-  const scrollTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+  const paginationButtons = [];
+  for (let i = 1; i <= 5; i++) {
+    paginationButtons.push(
+      <button
+        key={i}
+        onClick={() => {
+          setCurrentPage(i);
+          scrollMarket();
+        }}
+        className={classNames(
+          "inline-flex items-center px-4 pt-4 text-sm font-medium text-gray-500 border-t-2 border-transparent hover:border-gray-300 hover:text-gray-700 focus:border-t-indigo-500 focus:text-indigo-600",
+          currentPage === i &&
+            "border-indigo-500 text-indigo-600 focus:outline-none focus:text-indigo-800 focus:border-indigo-700"
+        )}
+      >
+        {i}
+      </button>
+    );
+  }
 
   return (
     <nav className="flex items-center justify-between px-4 mt-2 -mb-px border-t border-gray-200 sm:px-0">
-      <div
-        onClick={scrollMarket}
-        className="flex justify-center flex-1 w-0 -mt-px"
-      >
+      <div className="flex justify-center flex-1 w-0 -mt-px">
         {paginationButtons}
       </div>
     </nav>
