f4634eeb6ea626393fd883a8fd41beb01fe94eb0
MassyWeeWee <wyattconnollyfitness@gmail.com>
2023-04-20 17:17:37 -0700

    added type to facebook footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ const navigation = [
   {
     name: "Facebook",
     href: "https://www.facebook.com",
-    icon: (props) => (
+    icon: (props: any) => (
       <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
         <path
           fillRule="evenodd"
@@ -65,7 +65,7 @@ const navigation = [
 export default function Footer() {
   return (
     <footer className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+      <div className="px-6 py-12 mx-auto max-w-7xl md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
             <a
@@ -76,12 +76,12 @@ export default function Footer() {
               rel="noreferrer"
             >
               <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
+              <item.icon className="w-6 h-6" aria-hidden="true" />
             </a>
           ))}
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
-          <p className="text-center text-xs leading-5 text-gray-500">
+          <p className="text-xs leading-5 text-center text-gray-500">
             &copy; 2023 CryptoCrush, Inc. All rights reserved.
           </p>
           <p className="text-xs leading-5 text-gray-500">
